c9b3514b312816dd4eee8c4d5fc569b3f3bf2b96
Luiz Felipe <ruivolfb@gmail.com>
2020-12-31 15:04:26 -0300

    projeto-finalizado

---
+++ mysql/script.sql
@@ -0,0 +1,41 @@
+create table Bank(
+   id NUMERIC NOT NULL AUTO_INCREMENT,
+   name VARCHAR(100) NOT NULL,
+   description VARCHAR(200) NULL,
+   PRIMARY KEY (id)
+);
+
+
+create table Account(
+   id NUMERIC NOT NULL AUTO_INCREMENT,
+   id_bank NUMERIC NOT NULL,
+   account INT NOT NULL,
+   agency INT NOT NULL,
+   account_digit INT NOT NULL,
+   PRIMARY KEY ( id ),
+   FOREIGN KEY (id_bank) REFERENCES Bank(id)
+);
+
+
+create table Client(
+   id NUMERIC NOT NULL AUTO_INCREMENT,
+   id_account NUMERIC NOT NULL,
+   name VARCHAR(240) NOT NULL,
+   email VARCHAR(300) NOT NULL,
+   document VARCHAR(14) NOT NULL,
+   birth_date DATE,
+   PRIMARY KEY ( id ),
+   FOREIGN KEY (id_account) REFERENCES Account(id)
+);
+
+INSERT INTO orange.Bank
+(name, description)
+VALUES('Itau', 'banco top');
+
+INSERT INTO orange.Account
+(id_bank, account, agency, account_digit)
+VALUES(1, 123123, 1010, 2);
+
+INSERT INTO orange.Client
+(id_account, name, email, document, dt_birth_date)
+VALUES(1, 'Felipe', 'ruivolfb@gmail.com', '333.222.222-22', '2003-04-10');
